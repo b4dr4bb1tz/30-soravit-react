ebb7cd6f1924172d0445b0a96b30767290b61f74
Soravit Sakukumpat <dareal.u.ess@gmail.com>
2024-06-10 13:08:28 +0700

    change pic

--- src/components/owner.jsx
+++ src/components/owner.jsx
@@ -3,7 +3,7 @@ export default function OwnerPage() {
         <div className="flex flex-col h-screen bg-gray-300 items-center">
             <h1 className="text-3xl font-bold my-3">30_Soravit</h1>
             <div className="w-[400px] h-[400px] border border-black rounded-full overflow-hidden">
-                <img src="public\Batman-slapping-Robin-Meme-Blank.avif" alt="Soravit" className="object-cover w-full h-full" />
+                <img src="public\Batman-Pondering-meme-6.jpg" alt="Soravit" className="object-cover w-full h-full" />
             </div>
             <h2 className="my-5 text-xl font-bold">Short Biography</h2>
             <p>I am a Web Developer</p>
